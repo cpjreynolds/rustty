8f29ca00af1a009f3192e939ce9c7b85950db618
Cole Reynolds <cpjreynolds@gmail.com>
2015-05-01 15:48:04 -0400

    typing touchups. Preparing for major refactor for extensibility

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,29 +1,37 @@
-use nix::sys::ioctl::*;
+use nix;
+use nix::sys::ioctl;
 use nix::sys::termios;
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::os::unix::io::*;
+use std::result;
 
-pub use nix::Result;
+const TTYPATH: &'static str = "/dev/tty";
 
 pub struct Terminal {
     tty: File,
     ttyfd: RawFd,
-    size: Winsize,
+    size: ioctl::Winsize,
     orig_attr: termios::Termios,
 }
 
+pub type Result<T> = result::Result<T, Error>;
+
+pub enum Error {
+    Unknown,
+}
+
 impl Terminal {
-    pub fn new() -> Result<Terminal> {
+    pub fn new() -> nix::Result<Terminal> {
         let mut tmptty = OpenOptions::new()
             .read(true)
             .write(true)
-            .open("/dev/tty").unwrap();
+            .open(TTYPATH).unwrap();
         let tmpttyfd = tmptty.as_raw_fd();
         let mut tmpterm = Terminal {
             tty: tmptty,
             ttyfd: tmpttyfd,
-            size: Winsize::new(),
+            size: ioctl::Winsize::new(),
             orig_attr: termios::tcgetattr(tmpttyfd).unwrap(),
         };
         match tmpterm.update_size() {
@@ -32,15 +40,15 @@ impl Terminal {
         }
     }
 
-    pub fn destroy(self) -> Result<()> {
+    pub fn destroy(self) -> nix::Result<()> {
         match termios::tcsetattr(self.ttyfd, termios::SetArg::TCSANOW, &self.orig_attr) {
             Ok(()) => Ok(()),
             Err(error) => Err(error),
         }
     }
 
-    pub fn update_size(&mut self) -> Result<()> {
-        ioctl(self.tty.as_raw_fd(), TIOCGWINSZ(&mut self.size))
+    pub fn update_size(&mut self) -> nix::Result<()> {
+        ioctl::ioctl(self.tty.as_raw_fd(), ioctl::TIOCGWINSZ(&mut self.size))
     }
 
     pub fn rows(&self) -> u16 {
@@ -56,9 +64,9 @@ trait Constructor<T> {
     fn new() -> T;
 }
 
-impl Constructor<Winsize> for Winsize {
-    fn new() -> Winsize {
-        Winsize {
+impl Constructor<ioctl::Winsize> for ioctl::Winsize {
+    fn new() -> ioctl::Winsize {
+        ioctl::Winsize {
             ws_row: 0,
             ws_col: 0,
             ws_xpixel: 0,
