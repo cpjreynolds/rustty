a78a2cd4f1b0faa9cdaa1dccb14389c05771747d
Cole Reynolds <cpjreynolds@gmail.com>
2015-05-15 10:26:18 -0400

    setup orig tios

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,13 @@ extern crate bitflags;
 extern crate libc;
 extern crate nix;
 
+use std::io::prelude::*;
+use std::fs::OpenOptions;
+use std::os::unix::io::AsRawFd;
+
+use nix::sys::termios;
+
+
 static XTERM_FUNCS: &'static [&'static str] = &[
     "\x1b[?1049h",
     "\x1b[?1049l",
@@ -44,4 +51,22 @@ static XTERM_KEYS: &'static [&'static str] = &[
     "\x1bOD",
     "\x1bOC",
 ];
+
+pub fn init() -> Terminal {
+    let tty = OpenOptions::new()
+                          .read(true)
+                          .write(true)
+                          .open("/dev/tty")
+                          .unwrap();
+
+    
+
+    Terminal {
+        orig_tios: termios::tcgetattr(tty.as_raw_fd()).unwrap(),
+    }
+}
+
+pub struct Terminal {
+    orig_tios: termios::Termios,
+}
 
