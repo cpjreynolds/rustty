147d2ebd41c507aa76f423d652159663faa1382b
Cole Reynolds <cpjreynolds@gmail.com>
2015-05-15 11:36:27 -0400

    static builtin functions

--- src/lib.rs
+++ src/lib.rs
@@ -9,48 +9,7 @@ use std::os::unix::io::AsRawFd;
 
 use nix::sys::termios;
 
-
-static XTERM_FUNCS: &'static [&'static str] = &[
-    "\x1b[?1049h",
-    "\x1b[?1049l",
-    "\x1b[?12l\x1b[?25h",
-    "\x1b[?25l",
-    "\x1b[H\x1b[2J",
-    "\x1b(B\x1b[m",
-    "\x1b[4m",
-    "\x1b[1m",
-    "\x1b[5m",
-    "\x1b[7m",
-    "\x1b[?1h\x1b=",
-    "\x1b[?1l\x1b>",
-    "\x1b[?1000h",
-    "\x1b[?1000l",
-];
-
-static XTERM_KEYS: &'static [&'static str] = &[
-    "\x1bOP",
-    "\x1bOQ",
-    "\x1bOR",
-    "\x1bOS",
-    "\x1b[15~",
-    "\x1b[17~",
-    "\x1b[18~",
-    "\x1b[19~",
-    "\x1b[20~",
-    "\x1b[21~",
-    "\x1b[23~",
-    "\x1b[24~",
-    "\x1b[2~",
-    "\x1b[3~",
-    "\x1bOH",
-    "\x1bOF",
-    "\x1b[5~",
-    "\x1b[6~",
-    "\x1bOA",
-    "\x1bOB",
-    "\x1bOD",
-    "\x1bOC",
-];
+mod terminal;
 
 pub fn init() -> Terminal {
     let tty = OpenOptions::new()
@@ -58,8 +17,6 @@ pub fn init() -> Terminal {
                           .write(true)
                           .open("/dev/tty")
                           .unwrap();
-
-    
 
     Terminal {
         orig_tios: termios::tcgetattr(tty.as_raw_fd()).unwrap(),
