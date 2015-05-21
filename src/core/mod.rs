80615586dcf3fb842fbd1a0d7b7ae4071ae7d6b2
Cole Reynolds <cpjreynolds@gmail.com>
2015-05-21 19:07:56 -0400

    make docs show?

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,8 +1,8 @@
-mod terminal;
-mod device;
-mod cellbuffer;
-mod bytebuffer;
-mod cursor;
+pub mod terminal;
+pub mod device;
+pub mod cellbuffer;
+pub mod bytebuffer;
+pub mod cursor;
 
 pub use self::terminal::Terminal;
 pub use self::cellbuffer::{Cell, Style, Color, Attr};
