efc19b9a9aa555ca4b5e9a7d2b666d2894909878
Syntaf <gmercer015@gmail.com>
2015-10-07 15:20:17 -0400

    Circle example refactoring

--- examples/circle.rs
+++ examples/circle.rs
@@ -9,15 +9,13 @@ const BLOCK: char = '\u{25AA}';
 const BUFF_ALIGN: usize = 35;
 
 fn write_word(term: &mut Terminal, row: usize, word: String) { 
-
+    // Helper function to write right-aligned text to the passed
+    // row, aligns via the BUFF_ALIGN constant
     let cols = term.cols() as usize;
 
-    for (idx, ch) in word.chars().enumerate() {
-        
+    for (idx, ch) in word.chars().enumerate() { 
         let x = cols - BUFF_ALIGN;
-        
-        let mut cell = &mut term[(x as usize + idx, row)];
-        cell.set_ch(ch);
+        term[(x + idx, row)].set_ch(ch);
     }
 }
 
@@ -34,11 +32,14 @@ fn main() {
             }
         }
 
+        // Grab the size of the console, and reserve 4 rows at the bottom
+        // to write text to. These bottom 4 will NOT be rendered
         let (cols, rows) = term.size();
         let (cols, rows) = (cols as isize, (rows - 4) as isize);
 
         let (a, b) = (cols / 2, rows / 2);
 
+        // Main render loop, draws the circle
         for i in 0..cols*rows {
             let y = i as isize / cols;
             let x = i as isize % cols;
@@ -52,11 +53,14 @@ fn main() {
             }
         }
 
+        // Render text at bottom
         let i = cols*rows;
         let y = i as isize / cols;
         write_word(&mut term, y as usize, "+ -> Increase Radius".to_string());
         write_word(&mut term, (y+1) as usize, "- -> Decrease Radius".to_string());
         write_word(&mut term, (y+2) as usize, "q -> Quit".to_string());
+        
+        // Swap buffers
         term.swap_buffers().unwrap();
     }
 }
