b184bc3d4bb90c74ee9573cf5a902e67a0a61875
Syntaf <gmercer015@gmail.com>
2015-10-13 16:25:58 -0700

    Finish UI integration for circle example

--- examples/circle.rs
+++ examples/circle.rs
@@ -4,30 +4,19 @@ use rustty::{
     Terminal,
     Event,
     HasSize,
+    CellAccessor
 };
 
 use rustty::ui::{
     Painter,
     Dialog,
-    DialogResult,
+    Widget,
     Alignable,
     HorizontalAlign,
     VerticalAlign
 };
 
 const BLOCK: char = '\u{25AA}';
-const BUFF_ALIGN: usize = 35;
-
-fn write_word(term: &mut Terminal, row: usize, word: String) { 
-    // Helper function to write right-aligned text to the passed
-    // row, aligns via the BUFF_ALIGN constant
-    let cols = term.cols() as usize;
-
-    for (idx, ch) in word.chars().enumerate() { 
-        let x = cols - BUFF_ALIGN;
-        term[(x + idx, row)].set_ch(ch);
-    }
-}
 
 fn create_optiondlg() -> Dialog {
     let mut optiondlg = Dialog::new(50, 6);
@@ -45,9 +34,15 @@ fn create_optiondlg() -> Dialog {
 }
 
 fn main() {
+    // Create our terminal, dialog window and main canvas
     let mut term = Terminal::new().unwrap();
     let mut optiondlg = create_optiondlg();
+    let mut canvas = Widget::new(term.size().0, term.size().1 - 4);
+
+    // Align canvas to top left, and dialog to bottom right
     optiondlg.window_mut().align(&term, HorizontalAlign::Right, VerticalAlign::Bottom, 0);
+    canvas.align(&term, HorizontalAlign::Left, VerticalAlign::Top, 0);
+    
     let mut radius = 10u32;
     'main: loop {
         while let Some(Event::Key(ch)) = term.get_event(0).unwrap() {
@@ -58,20 +53,18 @@ fn main() {
                 _ => {},
             }
         }
-
-        // Grab the size of the console, and reserve 4 rows at the bottom
-        // to write text to. These bottom 4 will NOT be rendered
-        let (cols, rows) = term.size();
-        let (cols, rows) = (cols as isize, (rows - 4) as isize);
+        // Grab the size of the canvas
+        let (cols, rows) = canvas.size();
+        let (cols, rows) = (cols as isize, rows as isize);
 
         let (a, b) = (cols / 2, rows / 2);
 
-        // Main render loop, draws the circle
+        // Main render loop, draws the circle to canvas
         for i in 0..cols*rows {
             let y = i as isize / cols;
             let x = i as isize % cols;
 
-            let mut cell = &mut term[(x as usize, y as usize)];
+            let mut cell = canvas.get_mut(x as usize, y as usize).unwrap();
 
             if ((x - a).pow(2)/4 + (y - b).pow(2)) <= radius.pow(2) as isize {
                 cell.set_ch(BLOCK);
@@ -79,7 +72,9 @@ fn main() {
                 cell.set_ch(' ');
             }
         }
-        
+
+        // draw the canvas, dialog window and swap buffers
+        canvas.draw_into(&mut term);
         optiondlg.window().draw_into(&mut term);
         term.swap_buffers().unwrap();
     }
