3a8b504ac6d74f770f5c99988237b735d33b918e
Cole Reynolds <cpjreynolds@gmail.com>
2016-06-24 11:12:59 -0400

    fix accidental ':wq' in docs

--- src/core/input.rs
+++ src/core/input.rs
@@ -1,7 +1,7 @@
 /// An input event.
 ///
 /// An `Event` represents a single event from the underying terminal. At the moment no further
-/// processing is done on events and raw escape sequences will also be passed as `Key`s.:wq
+/// processing is done on events and raw escape sequences will also be passed as `Key`s.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Event {
     Key(char),
